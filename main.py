103446a8f37492f73ae27df3e21ff5f3f968455d
jannisremm <128300711+jannisremm@users.noreply.github.com>
2025-07-07 14:54:34 +0200

    feat: :sparkles: changed main to show all tracks merged together
    created two lists that keep track of all coordinates, and changed the plot to only show the combined locations

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import pathlib
 
 
 def get_lon_lat_lists(root):
-    """Takes an xml tree and outputs a list of longitude and latitude"""
+    """Creates an xml tree and outputs a list of longitude and latitude"""
+    tree = ET.parse(f"{track_file}")
+    root = tree.getroot()
 
     # Find the correct namespace for the gpx file
     namespace_search = re.search(r"\{(.*?)\}", root.tag)
@@ -30,25 +32,27 @@ def get_lon_lat_lists(root):
     return longitude_list, latitude_list, coordinates_list
 
 
-def plot_track(track_file):
-    print(track_file.name)
-
-    tree = ET.parse(f"{track_file}")
-    root = tree.getroot()
-
-    longitude_list, latitude_list, coordinates_list = get_lon_lat_lists(root)
+def plot_track(longitude_list, latitude_list):
 
     fig = plt.figure()
     ax = plt.axes(projection=ccrs.Mercator())
 
     ax.scatter(longitude_list, latitude_list, transform=ccrs.PlateCarree(), s=1)
 
-    plt.title(f"GPX Tracks {track_file.name}")
+    plt.title(f"GPX Tracks")
     plt.show()
 
 
 if __name__ == "__main__":
+    complete_longitudes = []
+    complete_latitudes = []
 
     for track_file in pathlib.Path("tracks").iterdir():
         if track_file.is_file():
-            plot_track(track_file)
+            longitude_list, latitude_list, coordinates_list = get_lon_lat_lists(
+                track_file
+            )
+            complete_longitudes.extend(longitude_list)
+            complete_latitudes.extend(latitude_list)
+            #  plot_track(longitude_list, latitude_list)
+    plot_track(complete_longitudes, complete_latitudes)
