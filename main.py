6b03eb4690fbc5a628ba15f581bf8146eab4b96b
jannisremm <128300711+jannisremm@users.noreply.github.com>
2025-07-07 13:51:09 +0200

    refactor: :recycle: turned plot_track into own function
    made main more readable by moving the plotting locig to its own function

--- main.py
+++ main.py
@@ -30,21 +30,25 @@ def get_lon_lat_lists(root):
     return longitude_list, latitude_list, coordinates_list
 
 
+def plot_track(track_file):
+    print(track_file.name)
+
+    tree = ET.parse(f"{track_file}")
+    root = tree.getroot()
+
+    longitude_list, latitude_list, coordinates_list = get_lon_lat_lists(root)
+
+    fig = plt.figure()
+    ax = plt.axes(projection=ccrs.Mercator())
+
+    ax.scatter(longitude_list, latitude_list, transform=ccrs.PlateCarree(), s=1)
+
+    plt.title(f"GPX Tracks {track_file.name}")
+    plt.show()
+
+
 if __name__ == "__main__":
 
     for track_file in pathlib.Path("tracks").iterdir():
-        if track_file.is_file():  # skip sub-directories
-            print(track_file.name)
-
-            tree = ET.parse(f"{track_file}")
-            root = tree.getroot()
-
-            longitude_list, latitude_list, coordinates_list = get_lon_lat_lists(root)
-
-            fig = plt.figure()
-            ax = plt.axes(projection=ccrs.Mercator())
-
-            ax.scatter(longitude_list, latitude_list, transform=ccrs.PlateCarree(), s=1)
-
-            plt.title("Multiple GPX Tracks (Geographically Accurate)")
-            plt.show()
+        if track_file.is_file():
+            plot_track(track_file)
